d991aa675d0308ee3e8ba10341e30016e53ab5e0
damian.deleon <damianmdeleon@gmail.com>
2021-03-04 15:02:46 -0600

    removed one line of code which was not necessary.

--- index.js
+++ index.js
@@ -91,7 +91,6 @@ Please see my github | Contact me for questions
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    console.log(license);
     if(license == "MIT"){
         return ('[![License: MIT](https://img.shields.io/badge/License-MIT-blue)](https://opensource.org/licenses/MIT)')
     } else if (license == "IBM") {
